a239b2dd2a2e9043c6e0bdc20935c8ec29f1509a
Ted Driggs <ted.driggs@outlook.com>
2023-07-13 16:29:19 -0700

    Don't end game on invalid input

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,18 @@ pub fn main() -> Result<(), Box<dyn Error>> {
 
         let mut next_move = String::new();
         std::io::stdin().read_line(&mut next_move)?;
-        game.submit(next_move.parse()?)?;
+
+        let next_mv = match next_move.parse() {
+            Ok(mv) => mv,
+            Err(e) => {
+                println!("Invalid move: {}", e);
+                continue;
+            }
+        };
+
+        if let Err(err) = game.submit(next_mv) {
+            println!("Illegal move: {}", err);
+        }
     }
 
     if let Some(victory) = game.outcome() {
