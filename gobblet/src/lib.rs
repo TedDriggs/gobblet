22969f6e1c17995245c8420fc590a4c20bfafc0c
Ted Driggs <ted.driggs@outlook.com>
2023-07-18 07:45:04 -0700

    Reexport errors from crate root

--- gobblet/src/lib.rs
+++ gobblet/src/lib.rs
@@ -4,8 +4,8 @@ mod game_move;
 mod piece;
 mod player;
 
-pub use board::{Board, Cell, CellState, Line};
+pub use board::{Board, Cell, CellError, CellState, Line};
 pub use game::{Game, SubmitMoveError, Victory};
-pub use game_move::Move;
+pub use game_move::{Move, ParseCellError, ParseMoveError};
 pub use piece::Size;
 pub use player::Player;
